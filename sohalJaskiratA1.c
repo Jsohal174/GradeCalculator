8be586f558f030925bb15557e7e3c04eccd86acc
Jaskirat Singh Sohal <119573762+Jsohal174@users.noreply.github.com>
2024-01-16 11:09:11 -0500

    Update sohalJaskiratA1.c

--- sohalJaskiratA1.c
+++ sohalJaskiratA1.c
@@ -1,19 +1,3 @@
-/************************sohalJaskiratA1.c**************
-Student Name: Jaskirat Singh Sohal      Email Id: jsohal03@uoguelph.ca
-Due Date:7 october                      Course Name: CIS 1300
-
-I have exclusive control over this submission via my password.
-By including this statement in this header comment, I certify that:
-
-1) I have read and understood the University policy on academic integrity. 2) I
-have completed the Computing with Integrity Tutorial on Moodle; and 3) I have
-achieved at least 80% in the Computing with Integrity Self Test.
-I assert that this work is my own. I have appropriately acknowledged any and
-all material that I have used, whether directly quoted or paraphrased.
-Furthermore, I certify that this assignment was prepared by me specifically for
-this course.
-********************************************************/
-
 /*********************************************************
 Compiling the program
 The program should be compiled using the following flags: -std=c99 -Wall
